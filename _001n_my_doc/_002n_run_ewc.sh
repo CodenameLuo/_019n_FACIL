fffc7aecb057dac5a6e70fc5121b6911d80bb4ad
LCY <congyuanyx@163.com>
2025-09-09 14:26:48 +0800

    同步

---
+++ _001n_my_doc/_002n_run_ewc.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd /home/HDD5/tjut_luocongyuan/_000n_00000000/_000n_uni/_003n_my_codes_projects/my_codes_projects/_002n_FACIL/FACIL/src
+
+python -m main_incremental \
+--exp-name run_ewc \
+--gpu 1 \
+--approach ewc \
+--datasets cifar100 \
+--num-tasks 4 \
+--nepochs 100 \
+--network resnet32 \
+--batch-size 64 \
+--lr 0.1 \
+--pin-memory true
